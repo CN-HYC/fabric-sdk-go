0d273ee512acd2f54310b52f92cb685d7a9d5408
CN-HYC <cn_yuchenhuang@163.com>
2025-10-31 10:40:07 +0800

    first commit

---
+++ fabric-sdk-go-1.0.1/pkg/client/channel/invoke/signature.go
@@ -0,0 +1,57 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package invoke
+
+import (
+	"github.com/qinleiyong/fabric-sdk-go/pkg/client/common/verifier"
+	"github.com/qinleiyong/fabric-sdk-go/pkg/common/providers/fab"
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+//NewSignatureValidationHandler returns a handler that validates an endorsement
+func NewSignatureValidationHandler(next ...Handler) *SignatureValidationHandler {
+	return &SignatureValidationHandler{next: getNext(next)}
+}
+
+//SignatureValidationHandler for transaction proposal response filtering
+type SignatureValidationHandler struct {
+	next Handler
+}
+
+//Handle for Filtering proposal response
+func (f *SignatureValidationHandler) Handle(requestContext *RequestContext, clientContext *ClientContext) {
+	fmt.Println("***** Test Client Handle *****")
+	//Filter tx proposal responses
+	err := f.validate(requestContext.Response.Responses, clientContext)
+	if err != nil {
+		requestContext.Error = errors.WithMessage(err, "signature validation failed")
+		return
+	}
+
+	// Delegate to next step if any
+	if f.next != nil {
+		f.next.Handle(requestContext, clientContext)
+	}
+}
+
+func (f *SignatureValidationHandler) validate(txProposalResponse []*fab.TransactionProposalResponse, ctx *ClientContext) error {
+	fmt.Println("***** Test Client validate *****")
+	for _, r := range txProposalResponse {
+		if err := verifyProposalResponse(r, ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func verifyProposalResponse(res *fab.TransactionProposalResponse, ctx *ClientContext) error {
+	fmt.Println("***** Test Client verifyProposalResponse *****")
+	sv := &verifier.Signature{Membership: ctx.Membership}
+	return sv.Verify(res)
+}
